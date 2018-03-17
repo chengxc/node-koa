8c2e8171305382aa7f19b4c1012020e7149abec4
Cheng Xiancheng <chengxc1990@outlook.com>
2018-03-17 17:06:37 +0800

    koa-02-中间件入门

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
 
+//注册一个中间件
+app.use(async (ctx,next)=>{
+    console.log("进入中间件",ctx.request.url);
+
+    //每一个中间件执行完毕都必须要执行next()从而让程序可以进入下一个中间件继续处理，否则程序将会发生异常
+    next();
+})
+
 app.use(async ctx => {
     ctx.body = 'Hello World';
 });
